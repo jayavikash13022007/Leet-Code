a2161c64c34ceff1eb50d6d7253a40f74b2b9c11
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-09 16:35:46 +0530

    Problem 345

---
+++ Java/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public String reverseVowels(String s) {
+        int left = 0;
+        int n = s.length();
+        int right = n-1;
+        char[] c = s.toCharArray();
+        char a;
+        ArrayList<Character> lst = new ArrayList<>(List.of('a','e','i','o','u','A','E','I','O','U'));
+        while(left<right){
+            while(left<n){
+                if(lst.contains(c[left])) break;
+                else left++;
+            }
+            while(right>-1){
+                if(lst.contains(c[right])) break;
+                else right--;
+            }
+            if(left>=right) break;
+            a = c[left];
+            c[left] = c[right];
+            c[right] = a;
+            left++;
+            right--;
+        }
+        String word = new String(c);
+        return word;
+    }
+} {
+    
+}
