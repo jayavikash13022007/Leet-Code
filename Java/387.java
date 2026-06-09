bdeadede234bee0c989875fb5e2a94c3dd5c47d8
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-09 19:56:52 +0530

    Problem 387

---
+++ Java/387.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> d = new HashMap<>();
+        ArrayList<Character> lst = new ArrayList<>();
+        ArrayList<Integer> idx = new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            try{
+                d.put(s.charAt(i),d.get(s.charAt(i)) + 1);
+            }
+            catch(Exception e){
+                d.put(s.charAt(i),1);
+                lst.add(s.charAt(i));
+                idx.add(i);
+            }
+        }
+        for(int i=0;i<lst.size();i++){
+            if(d.get(lst.get(i)) == 1) return idx.get(i); 
+        }
+        return -1;
+    }
+}
