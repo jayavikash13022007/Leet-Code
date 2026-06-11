acf26979487a2963d10145c8247204ce2259185e
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-11 09:16:19 +0530

    Problem 453

---
+++ Java/453.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int min = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            if(nums[i] < min) min = nums[i];
+        }
+        int total = 0;
+        for(int i = 0;i < nums.length;i++){
+            total += nums[i] - min;
+        }
+        return total;
+    }
+}
