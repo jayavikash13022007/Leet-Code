b7eefa372770d08695aaea1e84bdfba3727abe58
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-12 11:51:03 +0530

    Problem 384

---
+++ Java/384.java
@@ -0,0 +1,12 @@
+public class 384 extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1, stop = n, mid = 0;
+        while(start<=stop){
+            mid = start+(stop-start)/2;
+            if(guess(mid)==0) return mid;
+            else if(guess(mid)==1) start = mid+1;
+            else stop = mid - 1;
+        }
+        return -1;
+    }
+}
