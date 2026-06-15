84eda59da482b0dda44845c6b00fff88eea59d00
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-15 11:41:02 +0530

    Problem 1455

---
+++ Python/1455.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPrefixOfWord(self, sentence, searchWord):
+        sentence = sentence.split()
+        for i in range(len(sentence)):
+            if sentence[i].startswith(searchWord):
+                return i+1
+        return -1
