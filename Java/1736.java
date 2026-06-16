3dd5d4e7c300667e85d71237ea9191c8bb324f5d
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-16 16:28:58 +0530

    Problem 1736

---
+++ Java/1736.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String maximumTime(String time) {
+        StringBuilder s = new StringBuilder(time);
+        if(s.charAt(0) == '?' && s.charAt(1) == '?') s.replace(0,2,"23");
+        else{
+            if(s.charAt(0) == '?'){
+                if(s.charAt(1)>'3') s.setCharAt(0,'1');
+                else s.setCharAt(0,'2');
+            }
+            if(s.charAt(1) == '?'){
+                if(s.charAt(0) == '2') s.setCharAt(1,'3');
+                else s.setCharAt(1,'9');
+            }
+        }
+        if(s.charAt(3) == '?') s.setCharAt(3,'5');
+        if(s.charAt(4) == '?') s.setCharAt(4,'9');
+        return s.toString();
+    }
+}
