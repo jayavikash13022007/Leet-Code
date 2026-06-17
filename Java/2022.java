d0b1c51bef36b09f2e048e3be9925393302a7aca
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-17 15:01:27 +0530

    Problem 2022

---
+++ Java/2022.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if(original.length != m*n) return new int[][]{};
+        int[][] arr = new int[m][n];
+        for(int i = 0; i < original.length; i++) arr[i/n][i%n] = original[i];
+        return arr;
+    }
+}
