0461855a83f503bf01b8f25fe1ca6d2faf7b691e
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-17 21:55:44 +0530

    Problem 1688

---
+++ Java/1688.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int sum = 0;
+        while(n != 1){
+            if(n%2 == 0){
+                sum += n/2;
+                n /= 2;
+            }
+            else{
+                sum += (n-1)/2;
+                n = (n+1)/2;
+            }
+        }
+        return sum;
+    }
+}
