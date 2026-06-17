97d98ddd2d0a37bd80101e0114553d43b036a944
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-17 12:23:08 +0530

    Problem 46

---
+++ Python/46.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def permute(self, nums):
+        return recurse([],nums,[])
+def recurse(cur,arr,lst):
+    if len(arr) == 0:
+        if cur not in lst:
+            lst.append(cur)
+        return lst
+    for i in range(len(arr)):
+        recurse(cur + [arr[i]],arr[:i] + arr[i+1:],lst)
+    return lst
