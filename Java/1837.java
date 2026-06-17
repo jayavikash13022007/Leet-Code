d63a90d67fc5ef2022423be11532fd9f4650d865
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-17 22:13:12 +0530

    Problem 1837

---
+++ Java/1837.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int sumBase(int n, int k) {
+        int num = 0;
+        while(n != 0){
+            num += n%k;
+            n /= k;
+        }
+        return num;
+    }
+}
