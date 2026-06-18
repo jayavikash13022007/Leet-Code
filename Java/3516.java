a2ebfc1d3c7fec98f32f2eab6c587199fd37f749
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-18 11:10:16 +0530

    Problem 3516

---
+++ Java/3516.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int a = Math.abs(x-z), b = Math.abs(y-z);
+        if(a == b) return 0;
+        else if(a < b) return 1;
+        else return 2;
+    }
+}
