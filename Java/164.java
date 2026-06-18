4cb7f118e114a034a1b50d10d03aab1d98f3b3b1
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-18 16:02:35 +0530

    Problem 164

---
+++ Java/164.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        int max = 0;
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++){
+            max = Math.max(max,Math.abs(nums[i] - nums[i-1]));
+        }
+        return max;
+    }
+}
