b2ca2a5843d2c9b5303338d7a139709d5e9a8f41
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-18 13:51:16 +0530

    Problem 2485

---
+++ Java/2485.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int left = 0, right = n*(n+1)/2;
+        for(int i = 1; i <= n; i++){
+            left += i;
+            if(left==right) return i;
+            right -= i;
+        }
+        return -1;
+    }
+}
