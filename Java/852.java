153b241eadf544332043bdbbef9ca99239ed37c4
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-06-19 14:05:15 +0530

    Problem 852

---
+++ Java/852.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0, right = arr.length-1, mid = 0;
+        while(left<right){
+            mid = left + (right - left)/2;
+            if(arr[mid] < arr[mid+1]) left = mid+1;
+            else right = mid;
+        }
+        return left;
+    }
+}
